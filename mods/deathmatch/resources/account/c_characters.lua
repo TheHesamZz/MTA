a4c0bc261fe9455daebeb30a530103ee37d59dc1
Hesam Z.z <104154853+TheHesamZz@users.noreply.github.com>
2022-07-19 05:35:21 -0700

    Update c_characters.lua

--- mods/deathmatch/resources/account/c_characters.lua
+++ mods/deathmatch/resources/account/c_characters.lua
@@ -4,6 +4,7 @@
 * All rights reserved. This program and the accompanying materials are private property belongs to OwlGaming Community
 * Unauthorized copying of this file, via any medium is strictly prohibited
 * Proprietary and confidential
+:/
 * ***********************************************************************************************************************
 ]]
 
